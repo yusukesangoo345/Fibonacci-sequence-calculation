7fb9d7b3d61a1f67e6499cf26f454ecd01467dd8
yusukesangoo345 <yusuke.yt92@gmail.com>
2024-10-16 12:51:21 +0900

    Create index.py

---
+++ index.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[i-1] + fib_sequence[i-2])
+    return fib_sequence
+
+n = 10  # 任意の数列の長さ
+print(fibonacci(n))
